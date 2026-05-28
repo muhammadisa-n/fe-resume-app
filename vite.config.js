3acc5367123bca0c35f36c56077081dbd370d511
muhammadisa-n <muhammadisa226@gmail.com>
2026-05-28 16:12:31 +0700

    setup ci cd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASEPATH || "/",
+  };
 });
